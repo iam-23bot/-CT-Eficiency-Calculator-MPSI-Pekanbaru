0fae4d41a011c46ec6b2cc1ad8edbb9977d39388
Tukang Air <imamsudahdaritadi@gmail.com>
2025-06-28 09:32:00 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,20 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('cooling-tower-cache').then(function(cache) {
-      return cache.addAll(['index.html', 'manifest.json']);
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icons/icon-192.png'
+      ]);
     })
   );
+  self.skipWaiting(); // optional, force activate SW
 });
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(self.clients.claim()); // optional, immediately control pages
+});
+
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
